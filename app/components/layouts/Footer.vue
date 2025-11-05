c6f0eaf1dda6e8d699dbb6ec45e74c799e9a8db6
Kimcool <kimcool@me.com>
2025-11-05 16:45:05 +0800

    Update Footer.vue

--- app/components/layouts/Footer.vue
+++ app/components/layouts/Footer.vue
@@ -13,123 +13,24 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
       "
     >
       <a
-        href="https://sink.cool"
+        href="https://i.aff.dev"
         class="
           text-xl leading-none font-black text-gray-900 select-none
           dark:text-gray-100
         "
         :title="title"
-      >{{ title }}</a>
+      >AFF</a>
       <a
         class="
           mt-4 text-sm text-gray-500
           sm:mt-0 sm:ml-4 sm:border-l sm:border-gray-200 sm:pl-4
         "
-        href="https://html.zone"
+        href="https://i.aff.dev"
         target="_blank"
-        title="HTML.ZONE"
+        title="AFF"
       >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
+        &copy; {{ new Date().getFullYear() }} 部署于 Cloudflare Workers
       </a>
-      <span
-        class="
-          mt-4 inline-flex justify-center space-x-5
-          sm:mt-0 sm:ml-auto sm:justify-start
-        "
-      >
-        <a
-          v-if="email"
-          :href="`mailto:${email}`"
-          title="Email"
-          class="
-            text-gray-400
-            hover:text-gray-500
-          "
-        >
-          <span class="sr-only">{{ $t('layouts.footer.social.email') }}</span>
-          <GmailIcon
-            class="h-6 w-6"
-          />
-        </a>
-        <a
-          v-if="telegram"
-          :href="telegram"
-          target="_blank"
-          title="Telegram"
-          class="
-            text-gray-400
-            hover:text-gray-500
-          "
-        >
-          <span class="sr-only">{{ $t('layouts.footer.social.telegram') }}</span>
-          <TelegramIcon
-            class="h-6 w-6"
-          />
-        </a>
-        <a
-          v-if="blog"
-          :href="blog"
-          target="_blank"
-          title="Blog"
-          class="
-            text-gray-400
-            hover:text-gray-500
-          "
-        >
-          <span class="sr-only">{{ $t('layouts.footer.social.blog') }}</span>
-          <BloggerIcon
-            class="h-6 w-6"
-          />
-        </a>
-
-        <a
-          v-if="twitter"
-          :href="twitter"
-          target="_blank"
-          title="Twitter"
-          class="
-            text-gray-400
-            hover:text-gray-500
-          "
-        >
-          <span class="sr-only">{{ $t('layouts.footer.social.twitter') }}</span>
-          <XIcon
-            class="h-6 w-6"
-          />
-        </a>
-
-        <a
-          v-if="mastodon"
-          :href="mastodon"
-          target="_blank"
-          title="Mastodon"
-          class="
-            text-gray-400
-            hover:text-gray-500
-          "
-        >
-          <span class="sr-only">{{ $t('layouts.footer.social.mastodon') }}</span>
-          <MastodonIcon
-            class="h-6 w-6"
-          />
-        </a>
-
-        <a
-          v-if="github"
-          :href="github"
-          target="_blank"
-          title="GitHub"
-          class="
-            text-gray-400
-            hover:text-gray-500
-          "
-        >
-          <span class="sr-only">{{ $t('layouts.footer.social.github') }}</span>
-          <GitHubIcon
-            class="h-6 w-6"
-          />
-        </a>
-      </span>
     </div>
   </section>
 </template>
